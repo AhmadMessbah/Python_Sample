c51eeda99fd4f3f0271972f6f012a364e82f7df6
Messbah.A <messbah.a@gmail.com>
2024-05-19 17:28:16 +0330

    Updated

--- model/bl/person_bl.py
+++ model/bl/person_bl.py
@@ -1,2 +1,30 @@
+from model.da.da import DataAccess
+from model.entity.person import Person
+
+person_da = DataAccess(Person)
+
+
 class PersonBl:
-    pass
+    @staticmethod
+    def save(person):
+        return person_da.save(person)
+
+    @staticmethod
+    def edit(person):
+        return person_da.edit(person)
+
+    @staticmethod
+    def remove(id):
+        return person_da.remove(id)
+
+    @staticmethod
+    def find_all():
+        return person_da.find_all()
+
+    @staticmethod
+    def find_by_id(id):
+        return person_da.find_by_id(id)
+
+    @staticmethod
+    def find_by_family(family):
+        return person_da.find_by(Person.family == family)
---
+++ test/person_test.py
@@ -0,0 +1,3 @@
+from controller.person_controller import PersonController
+
+print(PersonController.remove(2))
