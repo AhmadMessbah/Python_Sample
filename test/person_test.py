b3e237c206ab2aa4cdc70735b6be562f81de15ad
Messbah.A <messbah.a@gmail.com>
2024-05-21 15:07:17 +0330

    updated

--- test/person_test.py
+++ test/person_test.py
@@ -3,9 +3,11 @@ from controller.person_controller import PersonController
 # print(PersonController.save("ahmad", "messbah"))
 # print(PersonController.edit(1, "ali", "messbah"))
 # print(PersonController.remove(1))
-# print(PersonController.find_all("messbah"))
-# print(PersonController.find_by_id(1))
-# print(PersonController.find_by_family("messbah"))
+# print(PersonController.find_all())
+# print(PersonController.find_by_id(10))
+# print(PersonController.find_by_family("messbah1"))
 
 
-print(PersonController.edit(100, "ali", "messbah"))
+# print(PersonController.edit(1, "ali", "messbah"))
+
+
